c1ad9b411b405e74b57355d202406b0438f46e97
Desmond Stular <dstular@yahoo.com>
2024-01-25 13:38:32 -0700

    Added gh pages package with deployment

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ function App() {
 
   useEffect(() => {
     const fetchMonsters = async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      const monsters = await response.json();
-      setMonsters(monsters);
+      const response = await axios(
+          "https://jsonplaceholder.typicode.com/users"
+      );
+      setMonsters(response.data);
     };
     fetchMonsters();
   }, []);
